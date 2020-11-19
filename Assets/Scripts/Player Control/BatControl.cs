88de884840c493529be4b2651bdfc6c90a58ac99
mynameisfloog <48299534+mynameisfloog@users.noreply.github.com>
2020-11-18 22:37:31 -0500

    Initial commit

---
+++ Assets/Scripts/Player Control/BatControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BatControl : MonoBehaviour
+{
+    public InputAction mouse;
+    public GameObject player;
+
+    void Update()
+    {
+        Vector2 inputVector = mouse.ReadValue<Vector2>();
+        Vector3 finalVector = new Vector3();
+        Debug.Log(inputVector);
+        finalVector.x = inputVector.x;
+        transform.RotateAround(player.transform.position, Vector3.up, 20 * Time.deltaTime);
+    }
+
+    private void OnEnable() { mouse.Enable(); }
+    private void OnDisable() { mouse.Disable(); }
+
+}
---
+++ Assets/Scripts/Player Control/PlayerInput.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInput : MonoBehaviour
+{
+    public InputAction wasd;
+    public CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        // player movement
+        Vector2 inputVector = wasd.ReadValue<Vector2>();
+        Vector3 finalVector = new Vector3();
+        finalVector.x = inputVector.x; finalVector.z = inputVector.y;
+        controller.Move(finalVector * Time.deltaTime * 3.14f);
+    }
+
+    private void OnEnable() { wasd.Enable(); }
+    private void OnDisable() { wasd.Disable(); }
+}
