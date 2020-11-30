c9b917b5a9287a941edfa51504078d22a827cc4e
mynameisfloog <48299534+mynameisfloog@users.noreply.github.com>
2020-11-29 23:59:54 -0500

    Test build made
    Pitcher object made and ball pitching works

---
+++ Assets/Scripts/BallServe.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallServe : MonoBehaviour
+{
+    public GameObject ball;
+    [SerializeField] float ballTimer;
+    [SerializeField] float ballSpeed;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        ballTimer += Time.deltaTime;
+        if (ballTimer >= 5)
+        {
+            GameObject newBall = Instantiate(ball, transform.position, transform.rotation);
+            Rigidbody ballrb = newBall.GetComponent<Rigidbody>();
+            ballrb.AddForce(-transform.forward * ballSpeed);
+            ballTimer = 0f;
+        }
+    }
+}
--- Assets/Scripts/Player Control/BatControl.cs
+++ Assets/Scripts/Player Control/BatControl.cs
@@ -7,14 +7,15 @@ public class BatControl : MonoBehaviour
 {
     public InputAction mouse;
     public GameObject player;
+    [SerializeField] float speedMult;
 
     void Update()
     {
         Vector2 inputVector = mouse.ReadValue<Vector2>();
         Vector3 finalVector = new Vector3();
         finalVector.y = inputVector.x;
-        Debug.Log(finalVector);
-        transform.RotateAround(player.transform.position, finalVector, 5);
+        //Debug.Log(finalVector);
+        transform.RotateAround(player.transform.position, finalVector, speedMult);
     }
 
     private void OnEnable() { mouse.Enable(); }
